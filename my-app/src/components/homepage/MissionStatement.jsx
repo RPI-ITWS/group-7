2d0e74dcdae541f275b18137d08b01f4a3127926
Richard Plotka <gagnot2@rpi.edu>
2024-03-01 10:52:00 -0500

    Landing Page - Filler Text
    Layout all good, just need to fill in lorem ipsum with actual text.

---
+++ my-app/src/components/homepage/MissionStatement.jsx
@@ -0,0 +1,15 @@
+function Mission() {
+  return (
+    <div className="missionStatement">
+      <h3>OUR MISSION STATEMENT</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.
+        Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
+        mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
+        tellus.
+      </p>
+    </div>
+  );
+}
+
+export default Mission;
---
+++ my-app/src/components/homepage/SellingPointR.jsx
@@ -0,0 +1,24 @@
+function SellingPointR(params) {
+  return (
+    <>
+      <div className="sellingPointR">
+        <div className="sellingImgR">
+          <img
+            src={params.museumImg}
+            alt="Museum Interior"
+            width={params.imgWidth}
+          />
+          <p class="imgCaptionR">{params.museumName}</p>
+        </div>
+
+        <div className="sellingTextR">
+          <h4>{params.sellingTitle}</h4>
+          <p>{params.sellingText1}</p>
+          <p>{params.sellingText2}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SellingPointR;
