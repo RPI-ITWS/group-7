e3a72c4dca3151fdfc9c991d5e3f0d583f8b6216
OhhOhh7777 <112569602+OhhOhh7777@users.noreply.github.com>
2024-04-21 20:49:19 -0400

    Museum

--- my-app/src/components/MuseumForm.js
+++ my-app/src/components/MuseumForm.js
@@ -24,7 +24,7 @@ const MuseumForm = () => {
     const makeStampPublic = document.getElementById("checkbox2").checked;
 
     const data = {
-      meseumName: museumId,
+      museumName: museumId,
       dateVisited: dateVisited,
       verification: verificationCode,
       notes: thoughtsAndNotes,
