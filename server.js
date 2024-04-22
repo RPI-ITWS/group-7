bae60df982bcbb6c42c63420b06e288866a027d5
rosataRPI <rosata@rpi.edu>
2024-04-21 20:46:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -129,7 +129,8 @@ app.post("/museum/:uid", (req, res) => {
 
       const verification = client.db("Museo").collection("verificationCodes");
       let code = await verification.findOne({
-        museumName: req.body.museumName
+        // museumName: req.body.museumName
+        museumName: "LowAstro"
       });
       code = code.verificationCode;
       console.log(code);
