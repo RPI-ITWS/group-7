e532b0937b276488da1565e61dc03232d04c8cfa
es-glmpr <simche@rpi.edu>
2024-04-23 10:24:34 -0400

    mamma mia here we go again
    more text

--- my-app/src/components/homepage/HeroSection.jsx
+++ my-app/src/components/homepage/HeroSection.jsx
@@ -11,10 +11,10 @@ function Hero(params) {
           Have you ever wanted to track museum visits and collect stamps to
           remember all of your trips instead of forgetting where you put the
           tickets? Have you ever lost all of your precious belongings to a flood
-          or a fire? Don't trust a scrapbook, those suck, make it digital. Here
-          at Museo we provide you with a scrapbook for all your museum visits
-          that can never be lost or destroyed! Get out and go explore more
-          museums today!
+          or a fire and had no way to recover precious lost memories? Don't trust 
+          a paper scrapbook, those suck, make it digital. Here at Museo we provide 
+          you with a scrapbook for all your museum visits that can never be lost 
+          or destroyed! Get out and go explore more museums today!
         </p>
         <Link to="/LogIn">
           <button>SIGN UP</button>
